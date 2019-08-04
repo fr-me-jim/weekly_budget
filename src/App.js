960754a8d9b97d8ddcc43dc0e35055977fc5e2be
fr-me-jim <fr.me.jim@gmail.com>
2019-08-04 19:59:30 +0200

    initial layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+      <header>
+        <h1>Weekly Expense</h1>
+      </header>
     </div>
   );
 }
