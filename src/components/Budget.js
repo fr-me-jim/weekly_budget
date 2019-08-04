ba9d3a6c85992464759b1b500819cd4b069034e3
fr-me-jim <fr.me.jim@gmail.com>
2019-08-04 20:19:53 +0200

    + handleSubmit + error control + onChange handler

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,14 +1,39 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const Budget = () => {
+
+    //define state
+    const [ amount, setAmount ] = useState(0);
+    const [ error, setError ] = useState(false);
+
+    //validate budget
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //validation
+        if(amount < 1 || isNaN(amount)){
+            setError(true);
+            return;
+        }
+
+        //success in validation
+
+
+    }
+
     return ( 
         <Fragment>
             <h2>Put your Budget</h2>
-            <form>
+
+            {error ? <p className="alert alert-danger error">Invalid Budget!</p> : null}
+
+            <form
+                onSubmit={handleSubmit}
+            >
                 <input 
                     type="number" className="u-full-width"
                     placeholder="Add Budget"
-                    //onChange={}
+                    onChange={e => setAmount(parseInt(e.target.value), 10)}
                 />
                 <input 
                     type="submit" className="button-primary u-full-width"
